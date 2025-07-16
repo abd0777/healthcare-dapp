7fc661daaf0a2aa2fad5d2e4cd97240608b0f7b8
healthcare-dapp123 <majorproject5555@gmail.com>
2025-07-16 11:25:16 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import LandingPage from './pages/LandingPage.jsx'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <LandingPage />
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="sticky top-0 left-0 w-full z-50 bg-gray-800 p-4 text-white bg-white/5 backdrop-blur-md border-b-[1px] border-white/20">
+          <div className='text-2xl font-semibold'>Health Ledger</div>
+      </nav>
+
+      <h1 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight leading-tight text-center">
+        <span className="bg-gradient-to-r from-gray-500 via-slate-300 to-white bg-clip-text text-transparent drop-shadow-lg">
+          Revolutionizing Healthcare with Blockchain Technology
+        </span>
+      </h1>
+
+       
+
+   
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
