e53a9ae280e6849b8d8abd69d236a31189107121
healthcare-dapp123 <majorproject5555@gmail.com>
2025-07-21 13:54:17 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element = {<LandingPage />} />
-
+      <Route path="/register" element = {<RegisterPage />} />
+      <Route path="/login" element = {<LoginPage />} />
     </Routes>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,43 @@
 import React from 'react'
 import { Flex, Box, Button, HStack, Text } from "@chakra-ui/react"
+import { useNavigate } from 'react-router-dom'
+
 function Navbar() {
+  const navigate  = useNavigate();
   return (
     <div>
       <nav className="sticky top-0 left-0 w-full z-50 bg-gray-800 p-4 text-white bg-white/5 backdrop-blur-md border-b-[1px] border-white/20">
           
-          <Flex as="nav" px={6} py={3} align="center" justify="space-between">
+          <Flex as="nav" px={6} align="center" justify="space-between">
  
             <Text fontWeight="bold" fontSize="lg">Health Ledger</Text>
 
-            <HStack spacing={20} flex="1" justify="center">
+            <HStack spacing={4} flex="1" justify="center">
               <Text>About</Text>
               <Text>Features</Text>
               <Text>Contact</Text>
             </HStack>
 
             <HStack spacing={4}>
-              <Button variant="ghost">Login</Button>
-              <Button colorScheme="blue">Signup</Button>
+               <Button
+                  variant="outline"
+                  borderColor="white"
+                  color="white"
+                  _hover={{ bg: "whiteAlpha.200" }}
+                  onClick = {() => navigate('/login')}
+                >
+                  Login
+                </Button>
+              <Button colorScheme="blue" onClick={()=> navigate('/register')}>Register</Button>
             </HStack>
           </Flex>
 
       </nav>
-
-      
-
-       
-
-   
-
-
-
 
     </div>
   )
 }
 
 export default Navbar
+
+
